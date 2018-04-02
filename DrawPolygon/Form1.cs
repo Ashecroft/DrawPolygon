5d668b6dde3358209bd9b4e71e2044e194eaaac9
Сергей <voron172007@mail.ru>
2018-04-02 15:05:32 +0700

    Initial commit

---
+++ DrawPolygon/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DrawPolygon
+{
+    public partial class Form1 : Form
+    {
+        Graphics graph;
+        Polygon myPolygon = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            graph = pnlCanvas.CreateGraphics();
+
+            tslPointState.Text = "";
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (myPolygon != null && myPolygon.IsCompleted == false)
+            {
+                myPolygon.Points.Add(new Point(e.X, e.Y));
+                if (myPolygon.Points.Count > 1)
+                {
+                    myPolygon.DrawLine(graph);
+                }
+            }
+            if (myPolygon != null && myPolygon.IsCompleted == true)
+            {
+                if (myPolygon.CheckIsPointInside(new Point(e.X, e.Y)))
+                    tslPointState.Text = "Точка внутри многоугольника";
+                else
+                    tslPointState.Text = "Точка снаружи многоугольника";
+
+            }
+        }
+
+        private void panel1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (myPolygon != null)
+            {
+                myPolygon.Points.Add(new Point(e.X, e.Y));
+                if (myPolygon.Points.Count > 1)
+                {
+                    myPolygon.DrawLine(graph);
+                    myPolygon.Complete(graph);
+                    myPolygon.IsCompleted = true;
+                }
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (myPolygon != null)
+            {
+                graph.Clear(SystemColors.Control);
+            }
+            myPolygon = new Polygon();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    myPolygon.Save(saveFileDialog1.FileName);
+                    tslPointState.Text = "Полигон сохранен";
+
+                }
+                catch 
+                {
+                    tslPointState.Text = "Ошибка при сохранении!!!";
+                }
+                
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    myPolygon = Polygon.Load(openFileDialog1.FileName);
+                    myPolygon.DrawFull(graph);
+                    tslPointState.Text = "Полигон загружен";
+                }
+                catch
+                {
+                    tslPointState.Text = "Ошибка при загрузке!!!";
+                }
+
+            }
+        }
+    }
+}
---
+++ DrawPolygon/Polygon.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace DrawPolygon
+{
+    public class Polygon
+    {
+        bool isCompleted;
+        List<Point> points;
+
+        public bool IsCompleted
+        {
+            get { return isCompleted; }
+            set { isCompleted = value; }
+        }
+
+        public List<Point> Points
+        {
+            get { return points; }
+            set { points = value; }
+        }
+
+        public Polygon()
+        {
+            IsCompleted = false;
+            points = new List<Point>();
+        }
+
+        public void DrawLine(Graphics graph)
+        {
+            int pointCount = Points.Count;
+            graph.DrawLine(Pens.Black, Points[pointCount - 1], Points[pointCount - 2]);
+        }
+
+        public void Complete(Graphics graph)
+        {
+            int pointCount = Points.Count;
+            graph.DrawLine(Pens.Black, Points[pointCount - 1], Points[0]);
+        }
+
+        public bool CheckIsPointInside(Point point)
+        {
+            bool result = false;
+            int j = Points.Count - 1;
+            for (int i = 0; i < Points.Count; i++)
+            {
+
+                if ((((Points[i].Y <= point.Y) && (point.Y < Points[j].Y)) || ((Points[j].Y <= point.Y) && (point.Y < Points[i].Y))) &&
+                 (point.X > (Points[j].X - Points[i].X) * (point.Y - Points[i].Y) / (Points[j].Y - Points[i].Y) + Points[i].X))
+                { 
+                    result = !result; 
+                }
+
+                j = i;
+            }
+            return result;
+        }
+
+        public void Save(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Polygon));
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            serializer.Serialize(fs, this);
+            fs.Close();
+        }
+
+        public static Polygon Load(string path)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Polygon));
+            FileStream fs = new FileStream(path, FileMode.Open);
+            return (Polygon)serializer.Deserialize(fs);           
+        }
+
+        public void DrawFull(Graphics graph)
+        {
+            graph.Clear(SystemColors.Control);
+            int j = Points.Count - 1;
+            for (int i = 0; i < Points.Count; i++)
+            {
+                graph.DrawLine(Pens.Black, Points[i], Points[j]);
+                j = i;
+            }
+        }
+    }
+}
